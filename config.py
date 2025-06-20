d4d46b67ab2a2702d6478784c49f5be26818d03c
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-20 14:18:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = "8097096084:AAEzqRI-A22btXsMG3FKj3LGCUVzlG43wI4"
+    BOT_TOKEN = ""
     
     API_ID = 27765349
     
     API_HASH = "9df1f705c8047ac0d723b29069a1332b"
     ADMIN_ID = [1116405290]
     
-    DB_URL = "mongodb+srv://STAR10:<iamagamer>@cluster0.w2snkuc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = ""
     
     DB_NAME = "MY_BOT_DB"
     
